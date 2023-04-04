f61894a83a8e38664038cd69649ea8ee445cd19f
Pratiksha <pratikshabadheka27@gmail.com>
2023-04-04 07:05:37 +0530

    Sum.java added

--- Sum.java
+++ Sum.java
@@ -4,5 +4,15 @@ class Sum{
         int b = 5;
         int c = a + b;
         System.out.println("sum = " + c);
+        evenSum(6);
+        
+        
+    }
+    static void evenSum(int j){
+        for (int i = 0; i < j; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
